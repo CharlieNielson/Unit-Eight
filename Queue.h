7d7113e17aeb501b48da2558cda045d1206d571b
CharlieNielson <charlienielson@mail.weber.edu>
2024-11-06 08:59:44 -0700

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -2,27 +2,30 @@
 #include <ostream>
 using namespace std;
 
+template<class type>
 class Queue {
     public:
         Queue(); //Constructs an empty queue
-        ~Queue(); //Destructor to free up memory
-        bool isEmpty(); //Returns true if the stack is empty
-        void add(string data); //Adds an item to the end of the queue
+        //~Queue(); //Destructor to free up memory
+        type isEmpty(); //Returns true if the stack is empty
+        void add(type data); //Adds an item to the end of the queue
         void remove(); // Removes the item at the front of the queue
-        string peek(string); // Returns the value in the front of the queue (without removing)
+        type peek(type); // Returns the value in the front of the queue (without removing)
         //ostream &operator << (ostream &out, const Queue &s); //Overloads the extraction operator to display the queue
         void present();//My own little function to display the entirety of the data stored.
     private:
         //Hello!
-        string thing[0];
+        type thing[100000];
         int length;
 };
 
-Queue::Queue() {
+template<class type>
+Queue<type>::Queue() {
     length = 0;
 }
 
-bool Queue::isEmpty() {
+template<class type>
+type Queue<type>::isEmpty() {
     if (length > 0) {
         return false;
     } else {
@@ -30,23 +33,27 @@ bool Queue::isEmpty() {
     }
 }
 
-void Queue::add(string data) {
-    thing[length] = data;
+template<class type>
+void Queue<type>::add(type data) {
     length++;
+    thing[length - 1] = data;
 }
 
-void Queue::remove() {
+template<class type>
+void Queue<type>::remove() {
     for (int i = 0; i < length; i++) {
         thing[i] = thing[i + 1];
     };
     length--;
 }
 
-string Queue::peek(string useless) {
+template<class type>
+type Queue<type>::peek(type useless) {
     return thing[0];
 }
 
-void Queue::present() {
+template<class type>
+void Queue<type>::present() {
     cout << "(";
     for (int i = 0; i < length; i++) {
         cout << thing[i];

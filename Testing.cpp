aef36a5097f27a34552201bd5904976e95df15e8
CharlieNielson <charlienielson@mail.weber.edu>
2024-11-06 09:23:07 -0700

    Update Testing.cpp

--- Testing.cpp
+++ Testing.cpp
@@ -6,12 +6,16 @@ using namespace std;
 int main() {
     Queue<string> data;
 
+    cout << data.isEmpty() << endl;
     data.add("One");
     data.add("Two");
     data.add("Three");
     data.add("Four");
     data.add("Five");
     data.present();
+    data.remove();
+    data.present();
+
 
     return 17;
 }
